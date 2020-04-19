bee0a24b4c70bcf099ac2163afe5ca0ba68472c2
Nick Barrash <nickbarrash@gmail.com>
2020-04-19 17:49:42 -0400

    v5.0.1 fix wall spikes

--- project/tom-braider/Assets/WallSpike.cs
+++ project/tom-braider/Assets/WallSpike.cs
@@ -2,29 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class WallSpike : MonoBehaviour
 {
     public bool Inverse = false;
 
-    public float ExtrudeDuration = 0.05f;
-    public float RetractDuration = 0.75f;
-    public float RetractedPauseDuration = 0.1f;
-    public float ExtrudedPauseDuration = 0.1f;
-    public float CycleOffset = 0f;
+    //public float ExtrudeDuration = 0.05f;
+    //public float RetractDuration = 0.75f;
+    //public float RetractedPauseDuration = 0.1f;
+    //public float ExtrudedPauseDuration = 0.1f;
+    //public float CycleOffset = 0f;
 
-    private float ExtrudeTimer = 0f;
-    private float RetractTimer = 0f;
-    private float RetractedPauseTimer = 0f;
-    private float ExtrudedPauseTimer = 0f;
-    private float CycleTimer = 0f;
+    //private float ExtrudeTimer = 0f;
+    //private float RetractTimer = 0f;
+    //private float RetractedPauseTimer = 0f;
+    //private float ExtrudedPauseTimer = 0f;
+    //private float CycleTimer = 0f;
+
+    //private float SynchroTimer = 0f;
 
     private float SPIKE_X_MOVE = -1f; // hard coded!!
     private float InitialX;
 
-    private bool IsStart = true;
+    //private bool IsStart = true;
+
+    //private bool SynchronizeNextCycle = false;
+    //private float SynchronizeInterval = 30;
+    //private float SyncornizeTriggerTimer = 0f;
+
 
     void Start() {
-        CycleTimer = CycleOffset;
         InitialX = transform.localPosition.x;
 
         if (Inverse) {
@@ -32,51 +39,88 @@ public class WallSpike : MonoBehaviour
                 child.localPosition = new Vector3(child.localPosition.x * -1f, child.localPosition.y, child.localPosition.z);
             }
         }
+
+        SetSpikeFromT(0f);
     }
+
+    //void ResetTimerValues() {
+    //    ExtrudeTimer = 0f;
+    //    RetractTimer = 0f;
+    //    RetractedPauseTimer = 0f;
+    //    ExtrudedPauseTimer = 0f;
+    //    CycleTimer = 0f;
+    //}
+
+    //void Synchronize() {
+    //    Debug.Log("Synchronize " + gameObject.name);
+    //    ResetTimerValues();
+    //    float SyncTimeInterval = ExtrudeDuration + RetractDuration + RetractedPauseDuration + ExtrudedPauseDuration;
+    //    float SynchroTimer = Time.time % SyncTimeInterval;
+    //    SynchronizeNextCycle = false;
+    //    SyncornizeTriggerTimer = SynchronizeInterval;
+    //}
 
     // Update is called once per frame
-    void Update()
-    {
-        if (CycleTimer > 0f || IsStart) {
-            IsStart = false;
-            CycleTimer -= Time.deltaTime;
-            if (CycleTimer <= 0f) {
-                RetractTimer = RetractDuration - CycleTimer;
-                CycleTimer = 0f;
-            }
-        } else if (RetractTimer > 0f) {
-            RetractTimer -= Time.deltaTime;
-            Retract();
-            if (RetractTimer <= 0f) {
-                RetractedPauseTimer = RetractedPauseDuration - RetractTimer;
-                RetractTimer = 0f;
-            }
-        } else if (RetractedPauseTimer > 0f) {
-            RetractedPauseTimer -= Time.deltaTime;
-            if (RetractedPauseTimer <= 0f) {
-                ExtrudeTimer = ExtrudeDuration - RetractedPauseTimer;
-                RetractedPauseTimer = 0f;
-            }
-        } else if (ExtrudeTimer > 0f) {
-            ExtrudeTimer -= Time.deltaTime;
-            Extrude();
-            if (ExtrudeTimer <= 0f) {
-                ExtrudedPauseTimer = ExtrudedPauseDuration - ExtrudeTimer;
-                ExtrudeTimer = 0f;
-            }
-        } else if (ExtrudedPauseTimer > 0f) {
-            ExtrudedPauseTimer -= Time.deltaTime;
-            if (ExtrudedPauseTimer <= 0f) {
-                RetractTimer = RetractDuration - ExtrudedPauseTimer;
-                ExtrudedPauseTimer = 0f;
-            }
-        }
-    }
+    //void Update()
+    //{
+    //    if (SynchroTimer <= 0) {
+    //        if (CycleTimer > 0f || IsStart) {
+    //            IsStart = false;
+    //            CycleTimer -= Time.deltaTime;
+    //            if (CycleTimer <= 0f) {
+    //                RetractTimer = RetractDuration - CycleTimer;
+    //                CycleTimer = 0f;
+    //            }
+    //        } else if (RetractTimer > 0f) {
+    //            RetractTimer -= Time.deltaTime;
+    //            Retract();
+    //            if (RetractTimer <= 0f) {
+    //                RetractedPauseTimer = RetractedPauseDuration - RetractTimer;
+    //                RetractTimer = 0f;
+    //            }
+    //        } else if (RetractedPauseTimer > 0f) {
+    //            if (SynchronizeNextCycle) {
+    //                Synchronize();
+    //            } else {
+    //                RetractedPauseTimer -= Time.deltaTime;
+    //                if (RetractedPauseTimer <= 0f) {
+    //                    ExtrudeTimer = ExtrudeDuration - RetractedPauseTimer;
+    //                    RetractedPauseTimer = 0f;
+    //                }
+    //            }
+    //        } else if (ExtrudeTimer > 0f) {
+    //            ExtrudeTimer -= Time.deltaTime;
+    //            Extrude();
+    //            if (ExtrudeTimer <= 0f) {
+    //                ExtrudedPauseTimer = ExtrudedPauseDuration - ExtrudeTimer;
+    //                ExtrudeTimer = 0f;
+    //            }
+    //        } else if (ExtrudedPauseTimer > 0f) {
+    //            ExtrudedPauseTimer -= Time.deltaTime;
+    //            if (ExtrudedPauseTimer <= 0f) {
+    //                RetractTimer = RetractDuration - ExtrudedPauseTimer;
+    //                ExtrudedPauseTimer = 0f;
+    //            }
+    //        }
+    //    } else {
+    //        SynchroTimer -= Time.deltaTime;
+    //        if (SynchroTimer <= 0) {
+    //            CycleTimer = CycleOffset;
+    //            IsStart = true;
+    //            SynchroTimer = 0f;
+    //        }
+    //    }
 
-    public void Extrude() {
-        float t = Mathf.InverseLerp(ExtrudeDuration, 0, ExtrudeTimer);
-        SetSpikeFromT(t);
-    }
+    //    SyncornizeTriggerTimer -= Time.deltaTime;
+    //    if (SyncornizeTriggerTimer <= 0) {
+    //        SynchronizeNextCycle = true;
+    //    }
+    //}
+
+    //public void Extrude() {
+    //    float t = Mathf.InverseLerp(ExtrudeDuration, 0, ExtrudeTimer);
+    //    SetSpikeFromT(t);
+    //}
 
     public void SetSpikeFromT(float t) {
         transform.localScale = new Vector3(t, transform.localScale.y, transform.localScale.z);
@@ -85,8 +129,8 @@ public class WallSpike : MonoBehaviour
         transform.localPosition = new Vector3(InitialX + tmpT * SPIKE_X_MOVE, transform.localPosition.y, transform.localPosition.z);
     }
 
-    public void Retract() {
-        float t = Mathf.InverseLerp(0, RetractDuration, RetractTimer);
-        SetSpikeFromT(t);
-    }
+    //public void Retract() {
+    //    float t = Mathf.InverseLerp(0, RetractDuration, RetractTimer);
+    //    SetSpikeFromT(t);
+    //}
 }
---
+++ project/tom-braider/Assets/WallSpikeCoordinator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpikeCoordinator : MonoBehaviour
+{
+    public List<WallSpike> Spikes;
+
+    public float AnimationOffset;
+    public float RetractDuration;
+    public float RetractPauseDuration;
+    public float ExtrudeDuration;
+    public float ExtractPauseDuration;
+
+    public float CycleModulo(float Offset, float SetTime) {
+        return (SetTime + Offset) % (RetractDuration + RetractPauseDuration + ExtrudeDuration + ExtractPauseDuration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MyTime = Time.time;
+        for(int index = 0; index < Spikes.Count; index++) {
+            float CycleTime = CycleModulo(index * AnimationOffset, MyTime);
+
+            Debug.Log(CycleTime);
+
+            if (CycleTime > RetractDuration) {
+                CycleTime -= RetractDuration;
+                if (CycleTime > RetractPauseDuration) {
+                    CycleTime -= RetractPauseDuration;
+                    if (CycleTime > ExtrudeDuration) {
+                        CycleTime -= ExtrudeDuration;
+                        if (CycleTime > ExtractPauseDuration) {
+                            Debug.Log("Warn shouldn't get here");
+                        } else {
+                            Debug.Log("Retracted Pause");
+                            // Retracted Pause
+                            Spikes[index].SetSpikeFromT(1);
+                        }
+                    } else {
+                        // Retract
+                        Debug.Log("Retract");
+                        Spikes[index].SetSpikeFromT(RetractT(CycleTime));
+                    }
+                } else {
+                    // Extruded Pause
+                    Debug.Log("Extruded Pause");
+                    Spikes[index].SetSpikeFromT(0);
+                }
+            } else {
+                // Extrude
+                Debug.Log("Extrude");
+                Spikes[index].SetSpikeFromT(ExtrudeT(CycleTime));
+            }
+        }
+    }
+
+    public float ExtrudeT(float ExtrudeTime) {
+        return Mathf.InverseLerp(RetractDuration, 0, ExtrudeTime);
+    }
+
+    public float RetractT(float RetractTime) {
+        return Mathf.InverseLerp(0, ExtrudeDuration, RetractTime);
+    }
+}
